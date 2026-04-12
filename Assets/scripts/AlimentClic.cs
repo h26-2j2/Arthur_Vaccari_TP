6dd5cd1c7783a8f8c80539001ed131f95bd172a3
Arthur-Vaccari60 <e2535000@cmaisonneuve.qc.ca>
2026-04-12 19:21:14 -0400

    fin-du-tp

--- Assets/scripts/AlimentClic.cs
+++ Assets/scripts/AlimentClic.cs
@@ -1,16 +1,55 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class AlimentClic : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public JeuNiveau1 jeu;
+    public int indexAliment;
+    public Collider2D colliderMonstre;
+
+    private Vector3 positionInitiale;
+    private float zCamera;
+
     void Start()
     {
-        
+        positionInitiale = transform.position;
+        zCamera = Camera.main.WorldToScreenPoint(transform.position).z;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
-        
+        positionInitiale = transform.position;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 positionSouris = Input.mousePosition;
+        positionSouris.z = zCamera;
+
+        Vector3 positionMonde = Camera.main.ScreenToWorldPoint(positionSouris);
+        positionMonde.z = transform.position.z;
+
+        transform.position = positionMonde;
+    }
+
+    void OnMouseUp()
+    {
+        Collider2D monCollider = GetComponent<Collider2D>();
+
+        if (monCollider != null && colliderMonstre != null && monCollider.bounds.Intersects(colliderMonstre.bounds))
+        {
+            if (jeu != null)
+            {
+                jeu.VerifierReponse(indexAliment);
+            }
+
+            if (gameObject.activeSelf)
+            {
+                transform.position = positionInitiale;
+            }
+        }
+        else
+        {
+            transform.position = positionInitiale;
+        }
     }
 }
--- Assets/scripts/JeuNiveau1.cs
+++ Assets/scripts/JeuNiveau1.cs
@@ -1,16 +1,197 @@
+using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+[System.Serializable]
+public class AlimentInfo
+{
+    public string nom;
+    public GameObject objet;
+    public AudioClip sonNom;
+}
 
 public class JeuNiveau1 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Aliments")]
+    public AlimentInfo[] aliments;
+
+    [Header("UI")]
+    public TextMeshProUGUI textePoints;
+    public GameObject uiVictoire;
+    public GameObject uiDefaite;
+
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip sonBonneReponse;
+    public AudioClip sonMauvaiseReponse;
+    public AudioClip sonVictoire;
+    public AudioClip sonDefaite;
+
+    [Header("Paramètres")]
+    public int pointsPourGagner = 5;
+    public int erreursMax = 3;
+
+    private int points = 0;
+    private int erreurs = 0;
+    private int indexDemande = -1;
+    private bool partieTerminee = false;
+
     void Start()
     {
-        
+        points = 0;
+        erreurs = 0;
+        partieTerminee = false;
+
+        if (uiVictoire != null)
+        {
+            uiVictoire.SetActive(false);
+        }
+
+        if (uiDefaite != null)
+        {
+            uiDefaite.SetActive(false);
+        }
+
+        MettreAJourPoints();
+        NouvelleDemande();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (partieTerminee)
+        {
+            return;
+        }
+
+        // Appuie sur espace pour réentendre la demande
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            JouerDemande();
+        }
+    }
+
+    void MettreAJourPoints()
+    {
+        if (textePoints != null)
+        {
+            textePoints.text = "Points : " + points;
+        }
+    }
+
+    void NouvelleDemande()
+    {
+        List<int> alimentsDisponibles = new List<int>();
+
+        for (int i = 0; i < aliments.Length; i++)
+        {
+            if (aliments[i].objet != null && aliments[i].objet.activeSelf)
+            {
+                alimentsDisponibles.Add(i);
+            }
+        }
+
+        if (alimentsDisponibles.Count == 0)
+        {
+            Gagner();
+            return;
+        }
+
+        indexDemande = alimentsDisponibles[Random.Range(0, alimentsDisponibles.Count)];
+        JouerDemande();
+    }
+
+    void JouerDemande()
+    {
+        if (indexDemande < 0 || indexDemande >= aliments.Length)
+        {
+            return;
+        }
+
+        if (audioSource != null && aliments[indexDemande].sonNom != null)
+        {
+            audioSource.PlayOneShot(aliments[indexDemande].sonNom);
+        }
+    }
+
+    public void VerifierReponse(int indexChoisi)
+    {
+        if (partieTerminee)
+        {
+            return;
+        }
+
+        if (indexChoisi < 0 || indexChoisi >= aliments.Length)
+        {
+            return;
+        }
+
+        if (indexChoisi == indexDemande)
+        {
+            points++;
+            MettreAJourPoints();
+
+            if (audioSource != null && sonBonneReponse != null)
+            {
+                audioSource.PlayOneShot(sonBonneReponse);
+            }
+
+            if (aliments[indexChoisi].objet != null)
+            {
+                aliments[indexChoisi].objet.SetActive(false);
+            }
+
+            if (points >= pointsPourGagner)
+            {
+                Gagner();
+            }
+            else
+            {
+                Invoke(nameof(NouvelleDemande), 0.6f);
+            }
+        }
+        else
+        {
+            erreurs++;
+
+            if (audioSource != null && sonMauvaiseReponse != null)
+            {
+                audioSource.PlayOneShot(sonMauvaiseReponse);
+            }
+
+            if (erreursMax > 0 && erreurs >= erreursMax)
+            {
+                Perdre();
+            }
+        }
+    }
+
+    void Gagner()
+    {
+        partieTerminee = true;
+
+        if (uiVictoire != null)
+        {
+            uiVictoire.SetActive(true);
+        }
+
+        if (audioSource != null && sonVictoire != null)
+        {
+            audioSource.PlayOneShot(sonVictoire);
+        }
+    }
+
+    void Perdre()
+    {
+        partieTerminee = true;
+
+        if (uiDefaite != null)
+        {
+            uiDefaite.SetActive(true);
+        }
+
+        if (audioSource != null && sonDefaite != null)
+        {
+            audioSource.PlayOneShot(sonDefaite);
+        }
     }
 }
